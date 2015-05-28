17dd6ed3475e7e709fb935607e8b4223ddcc1b8b
Dmitry Bespalov <dmitry.bespalov@zalando.de>
2015-05-28 12:03:59 +0200

    Fixed bug

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,6 +1,6 @@
 module CocoaPodsLastWord
   Pod::HooksManager.register('cocoapods-last_word', :post_install) do |context, user_options|
     hook = user_options[:hook] || user_options["hook"]
-    hook.call if hook.responds_to?(:call)
+    hook.call if hook.respond_to?(:call)
   end
 end
